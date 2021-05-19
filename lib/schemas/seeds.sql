0ef7148df85a2c4f036533aca2f95b1c98ac1bc8
Punkinut <heypunkinut@gmail.com>
2021-05-19 14:47:56 +1000

    Developed class the can add data to the department section

--- lib/schemas/seeds.sql
+++ lib/schemas/seeds.sql
@@ -9,3 +9,8 @@ VALUES ('Finance', 220000);
 
 INSERT INTO department (name, utilized_budget)
 VALUES ('Legal', 300000);
+
+-- General delete syntax
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM department WHERE name='Tim';
+SET SQL_SAFE_UPDATES = 1;
