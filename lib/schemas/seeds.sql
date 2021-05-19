30ff6854748d81f0d9661a39028724a109699e27
Punkinut <heypunkinut@gmail.com>
2021-05-19 15:33:37 +1000

    Updated general delete syntax

--- lib/schemas/seeds.sql
+++ lib/schemas/seeds.sql
@@ -12,5 +12,5 @@ VALUES ('Legal', 300000);
 
 -- General delete syntax
 SET SQL_SAFE_UPDATES = 0;
-DELETE FROM department WHERE name='Tim';
+DELETE FROM employee WHERE id=1;
 SET SQL_SAFE_UPDATES = 1;
