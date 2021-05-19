6c5609376eb9be8bf4f4dc7d7c4234c50785f52f
Punkinut <heypunkinut@gmail.com>
2021-05-19 13:48:23 +1000

    Added basic schema details and seed information for all departments

---
+++ lib/schemas/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (name, utilized_budget)
+VALUES ('Sales', 450000);
+
+INSERT INTO department (name, utilized_budget)
+VALUES ('Engineering', 420000);
+
+INSERT INTO department (name, utilized_budget)
+VALUES ('Finance', 220000);
+
+INSERT INTO department (name, utilized_budget)
+VALUES ('Legal', 300000);
