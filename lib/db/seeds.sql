2b73b48478c854a4c8a886a3427c4b6472e3fad5
Punkinut <heypunkinut@gmail.com>
2021-05-21 09:15:43 +1000

    Added all seed data for roles

--- lib/db/seeds.sql
+++ lib/db/seeds.sql
@@ -11,6 +11,30 @@ VALUES ('Finance', 220000);
 INSERT INTO department (name, utilized_budget)
 VALUES ('Legal', 300000);
 
+INSERT INTO role (title, salary, department_id)
+VALUES('Sales Manager', 280.000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES('Sales Representative', 170.000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES('Engineering Manager', 180.000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES('Software Engineer', 120.000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES('Accounting Manager', 120.00, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES('Accountant', 100.000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES('Lawyer', 120.000, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES('Attorney', 130.000, 4);
+
 -- General delete syntax
 SET SQL_SAFE_UPDATES = 0;
 DELETE FROM employee WHERE id=1;
