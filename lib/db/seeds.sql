31e66c71846f5fa965f59f2aff0206275edb7c5f
Punkinut <heypunkinut@gmail.com>
2021-05-21 14:42:11 +1000

    Cleaned up seeds file

--- lib/db/seeds.sql
+++ lib/db/seeds.sql
@@ -1,38 +1,20 @@
 -- Inserts into department column
 INSERT INTO department (name, utilized_budget)
-VALUES ('Sales', 450000);
-
-INSERT INTO department (name, utilized_budget)
-VALUES ('Engineering', 420000);
-
-INSERT INTO department (name, utilized_budget)
-VALUES ('Finance', 220000);
-
-INSERT INTO department (name, utilized_budget)
-VALUES ('Legal', 300000);
+VALUES 
+('Sales', 450000),
+('Engineering', 420000),
+('Finance', 220000),
+('Legal', 300000);
 
 INSERT INTO role (title, salary, department_id)
-VALUES('Sales Manager', 280000, 1);
-
-INSERT INTO role (title, salary, department_id)
-VALUES('Sales Representative', 170000, 1);
-
-INSERT INTO role (title, salary, department_id)
-VALUES('Engineering Manager', 180000, 2);
-
-INSERT INTO role (title, salary, department_id)
-VALUES('Software Engineer', 120000, 2);
-
-INSERT INTO role (title, salary, department_id)
-VALUES('Accounting Manager', 120000, 3);
-
-INSERT INTO role (title, salary, department_id)
-VALUES('Accountant', 100000, 3);
-
-INSERT INTO role (title, salary, department_id)
-VALUES('Lawyer', 120000, 4);
-
-INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Manager', 280000, 1),
+('Sales Representative', 170000, 1),
+('Engineering Manager', 180000, 2),
+('Software Engineer', 120000, 2),
+('Accounting Manager', 120000, 3),
+('Accountant', 100000, 3),
+('Lawyer', 120000, 4),
 VALUES('Attorney', 130000, 4);
 
 -- General delete syntax
