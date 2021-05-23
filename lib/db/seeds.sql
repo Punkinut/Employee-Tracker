565f6ab2a24a1ca73185abc150cbf28b4b3d3626
Punkinut <heypunkinut@gmail.com>
2021-05-24 09:11:42 +1000

    Updated seeds sql file with employee row information

--- lib/db/seeds.sql
+++ lib/db/seeds.sql
@@ -17,6 +17,16 @@ VALUES
 ('Lawyer', 120000, 4),
 ('Attorney', 130000, 4);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Alec', 'Becker', 1, null),
+('Jake', 'Smith', 4, 1),
+('James', 'Dean', 3, null),
+('Kanye', 'West', 2, 3),
+('Rami ', 'Makel', 5, null),
+('Mr.', 'Ayo', 6, 7),
+('David', 'Impey', 5, null);
+
 -- General delete syntax
 SET SQL_SAFE_UPDATES = 0;
 DELETE FROM employee WHERE id=1;
